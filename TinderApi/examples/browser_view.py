ed8736b5159732b3d9d0d0e2ea742bde2842b9fd
Mr. WorldWide <108243503+cexyboy@users.noreply.github.com>
2022-08-26 21:39:37 +0200

    delete examples
    added new, tests

--- TinderApi/examples/browser_view.py
+++
@@ -1,4 +0,0 @@
-from TinderApi import Tinder
-
-tinder = Tinder(debug=True, x_auth_token="your_auth_token")
-tinder.browser_view()
--- TinderApi/examples/login.py
+++
@@ -1,9 +0,0 @@
-from TinderApi import Tinder
-
-
-tinder = Tinder(debug=True)
-tinder.login(
-    "your_phone_number",
-    "your_email",
-    store_auth_token=True
-)
